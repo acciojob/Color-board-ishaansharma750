15b4dcd0429138d5410ad99439acd76aae9b1c78
Suansh Budhiraja <43946155+ishaansharma750@users.noreply.github.com>
2023-04-19 01:21:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const container = document.querySelector('.container');
+
+for(let i = 0; i < 800; i++) {
+	const box = document.createElement('div');
+	box.className = 'square';
+	container.appendChild(box);
+}
